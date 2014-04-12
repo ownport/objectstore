9cab74359c4cfbcbb14b9723a896a816f13ba63e
ownport <ownport@gmail.com>
2014-04-12 19:00:08 +0300

    [+] template for unittests

---
+++ tests/test_objectstore.py
@@ -0,0 +1,17 @@
+
+import unittest
+import objectstore
+
+from pprint import pprint
+
+class TestObjectStore(unittest.TestCase):
+    ''' TestObjectStore
+    '''
+    
+    def test_hello_world(self):
+        pass
+
+
+
+            
+    
